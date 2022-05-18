1a348cdfdcf09e57e353f160391405a095a4b4c4
shawn <shawnang167@gmail.com>
2022-05-18 23:00:09 +0800

    feat: colly basics

--- moves.go
+++ moves.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
-	//"github.com/gocolly/colly"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("books.toscrape.com"),
+	)
 
+	c.OnHTML("title", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println(r.StatusCode)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://books.toscrape.com/")
 }
 
-
-func test () {
-	fmt.Println("Done")
-}
-
+// func test() {
+// 	fmt.Println("Done")
+// }
